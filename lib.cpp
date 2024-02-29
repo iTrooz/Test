64b8ff5b5ddd5c981f2e72a79ba2e3de77dc3a1e
iTrooz <hey@itrooz.fr>
2024-02-29 16:24:02 +0100

    test throwing error across dll boundary

--- lib.cpp
+++ lib.cpp
@@ -1,5 +1,7 @@
 #include "stdio.h"
+#include <stdexcept>
 
 int addition(int a, int b){
+    throw std::runtime_error("Hey");
     return a+b;
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "stdio.h"
 #include "lib.h"
+#include <stdexcept>
 
 int main(){
-   printf("%i\n", addition(3, 4));
+   try {
+      printf("%i\n", addition(3, 4));
+   } catch(const std::exception &e) {
+      printf("Caught error\n");
+   }
 }
